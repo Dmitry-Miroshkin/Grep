c7cb2e49556e60b279678dfce25d473c9ca2635f
Dmm <dmm@mail.ru>
2022-01-08 13:02:00 +0300

    class GrepInputStream added

---
+++ src/GrepInputStream.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class GrepInputStream  extends FilterInputStream {
+    String substring;
+    BufferedReader br;
+
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+    protected GrepInputStream(InputStream in, String substring) {
+        super(in);
+        this.br = new BufferedReader(new InputStreamReader(in));
+        this.substring = substring;
+
+    }
+
+    public final String readLine() throws IOException {
+        String line;
+        do {
+            line = br.readLine();
+        } while ((line != null) && line.indexOf(substring) == -1);
+        return line;
+    }
+}
